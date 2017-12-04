126d038242b54f6be4e1dee7c87e37ce9607eac4
Ryan Otto <rotto@drew.edu>
2017-12-04 14:52:24 -0500

    Track player coords
    * During player movement animation, we can now see player coordinates changing using a DomAttrModified event handler

--- scripts/touchcourse.js
+++ scripts/touchcourse.js
@@ -5,16 +5,20 @@ $(document).ready(function() {
 	canvas.height = img.height;
 	canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
 
+	 document.getElementById("player").addEventListener("DOMAttrModified", function(){
+	 	$('#coordinates').text(($('#player').css('left')) + ' ' + ($('#player').css('top')));
+	 });
+
 	$('#course').click(function(e) { 
-		$('#coordinates').text( e.pageY + ', ' + e.pageX ); 
+		//$('#coordinates').text( e.pageY + ', ' + e.pageX ); 
 		$(function(){ 
 			$('#player').animate({ left:e.pageX,top:e.pageY}, 
 				function(){
 					var pixelData = canvas.getContext('2d').getImageData(e.offsetX, e.offsetY, 1, 1).data;
-					alert(pixelData);
+					if(pixelData[0]==0 && pixelData[1]==0 && pixelData[2]==0) alert("You lose!");
 			});
 		});
 	});
 
-	
+
 });
