231514748b63b2ebe607946e4628b9877cd549d6
Ryan Otto <rotto@drew.edu>
2017-12-04 13:24:51 -0500

    Implement preliminary game logic (levels & movement landing spot tracking)
    * Changed image to circle
    
    * Touch Course script now keeps track of pixel color wherever the user has clicked, but only once the movement animation is finished
    
    * Added sample background to be used in tests for player win/loss/effect conditions

--- scripts/touchcourse.js
+++ scripts/touchcourse.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+	var img = document.getElementById('background');
+	var canvas = document.createElement('canvas');
+	canvas.width = img.width;
+	canvas.height = img.height;
+	canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
 
 	$('#course').click(function(e) { 
 		$('#coordinates').text( e.pageY + ', ' + e.pageX ); 
-		$('#player').css({ left:e.pageX,top:e.pageY} );
+		$(function(){ 
+			$('#player').animate({ left:e.pageX,top:e.pageY}, 
+				function(){
+					var pixelData = canvas.getContext('2d').getImageData(e.offsetX, e.offsetY, 1, 1).data;
+					alert(pixelData);
+			});
+		});
 	});
 
-
+	
 });
