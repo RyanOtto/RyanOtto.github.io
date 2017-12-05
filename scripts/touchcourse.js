b626d71b3458462f63f0a2a273bb1612e08e6e03
Ryan Otto <rotto@drew.edu>
2017-12-04 21:27:28 -0500

    Tracking changes, Player image fix

--- scripts/touchcourse.js
+++ scripts/touchcourse.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
 
-	$('#player').css({left:375,top:275});
+//Observer to track player movement
+var target = document.getElementById('player');
+var observer = new MutationObserver(function(mutations) {
+ mutations.forEach(function(mutation) {
+   if(mutation.type == 'attributes');
+   	track();
+ });    
+});
+var config = { attributes: true, childList: true, characterData: true };
+observer.observe(target, config);
+
+
+	$('#player').css({left:0,top:0});
 	var img = document.getElementById('background');
 	var canvas = document.createElement('canvas');
 	canvas.width = img.width;
 	canvas.height = img.height;
+	
 	canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
 
-	 $("#player").on("DOMAttrModified", function(e){
-	 	$('#coordinates').text(($('#player').css('left')) + ' ' + ($('#player').css('top')));
+	 function track(e){
 	 	var left = ($('#player').css('left'));
-	 	var top = ($('#player').css('top'))
-	 	var pixelData = canvas.getContext('2d').getImageData(parseInt(left), parseInt(top), 1, 1).data;
-	 	//alert(pixelData);
+	 	var top = ($('#player').css('top'));
+	 	var pixelData = canvas.getContext('2d').getImageData(parseFloat(left), parseFloat(top), 1, 1).data;
 	 	console.log(pixelData[0] + ' ' + pixelData[1] + ' ' + pixelData[2]);
-		if(pixelData[0]==0 && pixelData[1]==0 && pixelData[2]==0) console.log("You lose!" + pixelData[0] + ' ' + pixelData[1] + ' ' + pixelData[2]);
-	 });
+		if(pixelData[0]==195 && pixelData[1]==195 && pixelData[2]==195) console.log("You lose!" + pixelData[0] + ' ' + pixelData[1] + ' ' + pixelData[2]);
+	 }
 
 	$('#course').click(function(e) { 
-		//$('#coordinates').text( e.pageY + ', ' + e.pageX ); 
 		$(function(){ 
 			$('#player').animate({ left:e.pageX,top:e.pageY}); 
 		});
